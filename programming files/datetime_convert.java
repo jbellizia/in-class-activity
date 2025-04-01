6514a860082988e7f5fbb7c2bdb4c63b306569cf
timsmith2003 <148157082+timsmith2003@users.noreply.github.com>
2025-04-01 10:55:41 -0400

    Update datetime_convert.java

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -21,3 +21,32 @@ public class datetime_convert {
         System.out.println(formattedDate);
     }
 }
+
+// Description A clear and concise description of what the issue/bug is.
+
+// Steps to Reproduce
+//Useless function included
+// First step to reproduce the issue/bug
+// Second step to reproduce the issue/bug
+// Third step to reproduce the issue/bug
+// Expected Behavior A clear and concise description of what you expected to happen.
+
+// Actual Behavior A clear and concise description of what actually happened.
+
+// Screenshots If applicable, add screenshots to help explain the issue/bug.
+
+// Additional Information
+
+// Operating System:
+// Browser:
+// Version:
+// Any other relevant information
+// Possible Solution If you have any suggestions for a possible solution, please include them here.
+
+// Related Issues Are there any related issues? If so, please link to them here.
+
+// Priority How critical is this issue/bug? Low, medium, or high priority?
+
+// Reproducibility How often can this issue/bug be reproduced? Always, sometimes, or random?
+
+// Impact What is the impact of this issue/bug? Low, medium, or high?
